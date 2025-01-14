e6a79f82f62b611b5d897838e58ed1b7a0e7344f
josebuenoluis <buenojosefrancisco@gmail.com>
2025-01-14 21:09:26 +0000

    Actualizando

--- web/react/lapiton/front-lapiton/src/App.jsx
+++ web/react/lapiton/front-lapiton/src/App.jsx
@@ -3,13 +3,14 @@ import { Routes,Route } from 'react-router-dom'
 import './App.css'
 import Inicio from './componentes/rutas/inicio'
 import Novedades from './componentes/rutas/novedades'
+import './componentes/css/fonts.css'
 
 function App() {
   return (
     <>
       <div className="Aplicacion">
       <Routes>
-        <Route path="/inicio" element={ <Inicio /> } />
+        <Route path="/" element={ <Inicio /> } />
         <Route path="novedades" element={ <Novedades /> } />
       </Routes>
     </div>
---
+++ web/react/lapiton/front-lapiton/src/componentes/header/HeaderComponent.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/imagenLogoPiton.png'
+import '../css/HeaderComponent.css'
+
+function HeaderComponent() {
+  return (
+    <header>
+        <div className="logo-header">
+            <p>laPiton</p>
+            <img src={Logo} alt="logo" />
+        </div>
+        <nav>
+            <Link to="/">Inicio</Link>
+            <a href="#">Categorias</a>
+            <Link to="ranking">Ranking</Link>
+            <Link to="novedades">Novedades</Link>
+        </nav>
+    </header>
+  )
+}
+
+export default HeaderComponent
