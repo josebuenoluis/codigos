b2768c0d2a61eac8d481dada67499b28ba4f2d26
josebuenoluis <buenojosefrancisco@gmail.com>
2025-02-05 12:34:05 +0000

    Actualizando

--- docker/docker_ubuntu/volumesOdoo/addons/fcties/models/models.py
+++ docker/docker_ubuntu/volumesOdoo/addons/fcties/models/models.py
@@ -24,7 +24,7 @@ class Alumno(models.Model):
         ],string="Periodo de Práctica",required=True) # Periodo de práctica (abril o septiembre, obligatorio).
     nota_media = fields.Float(string="Nota Media",required=True) # Nota media (obligatorio).
     nota_media_texto = fields.Text(string="Nota media en texto",compute="_compute_calcular_media",store=True)
-
+    empresa_id = fields.Many2one("empresas.empresa",string="Empresa de practicas")
 
     # Nota media en formato texto (campo calculado como se indica más adelante).
     @api.depends("nota_media")
@@ -47,3 +47,4 @@ class Empresa(models.Model):
     telefono = fields.Integer(string="Teléfono",required=True) # Teléfono de contacto (obligatorio).
     correo_electronico = fields.Char(string="Correo electrónico",required=True) # Correo electrónico (obligatorio).
     direccion = fields.Char(string="Dirección",required=True)   # Dirección (obligatorio).
+    alumno_ids = fields.One2many("alumnos.alumno","empresa_id",string="Alumnos en practica")
