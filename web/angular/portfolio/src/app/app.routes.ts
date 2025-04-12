1f07a4db8186c0f1e4be87d9a28135944bb31614
josebuenoluis <buenojosefrancisco@gmail.com>
2025-04-12 03:50:01 +0100

    Actualizando

--- web/angular/portfolio/src/app/app.routes.ts
+++ web/angular/portfolio/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
-
+import { BlogComponent } from './components/blog/blog.component';
 export const routes: Routes = [
     {path:"",component:HomeComponent},
-    {path:"proyectos",component:ProjectsComponent}
+    {path:"proyectos",component:ProjectsComponent},
+    {path:"blog",component:BlogComponent}
 ];
