8d4e6085d691ec8e4c62aa960bbae91af1b56ee2
josebuenoluis <buenojosefrancisco@gmail.com>
2025-05-02 08:52:11 +0100

    Actualizando proyecto

--- python/flask/proyecto_final/routes/asistentes.py
+++ python/flask/proyecto_final/routes/asistentes.py
@@ -5,3 +5,7 @@ asistentes = Blueprint("asistentes",__name__)
 @asistentes.route("/asistentes/crear", methods=["GET"])
 def crear_asistente():
     return render_template("crear-asistente.html")
+
+@asistentes.route("/asistentes/modificar", methods=["GET"])
+def modificar_asistente():
+    return render_template("asistentes-modificar.html")
